4ea7893db2432ea804725566de519ebe4895b819
ReyDonovan <gennady.zamalaev@gmail.com>
2022-02-26 09:02:57 +0300

    DB/Misc: fix quest The Source of the Madness (26734)

---
+++ sql/updates/world/2022_02_26_00_world.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template_addon` SET `bytes2`=1, `auras`='' WHERE `entry` IN (942,978,979,939,941);
+UPDATE `creature_template` SET `unit_flags`=512 WHERE `entry` IN (942,978,979,939,941);
